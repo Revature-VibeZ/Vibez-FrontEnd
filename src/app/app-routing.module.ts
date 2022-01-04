6ffcf6a8409171f14c5b9686f9f492293bb39ca5
adarvin14 <amdarvin14@gmail.com>
2022-01-04 17:25:36 -0500

    add create-post pathing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { AuthGuard } from './helpers/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { PostFeedComponent } from './components/post-feed/post-feed.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'postfeed', component: PostFeedComponent}
+  { path: 'postfeed', component: PostFeedComponent},
+  { path: 'createpost', component: CreatePostComponent},
   // { path: '', component: HomeComponent, canActivate: [AuthGuard] } Example of how to add authguard
 
   // { path: '**', redirectTo: '' }
