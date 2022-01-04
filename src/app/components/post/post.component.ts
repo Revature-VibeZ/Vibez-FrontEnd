6c42168ef912801d6668cfd38e42185847ede07d
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2022-01-03 20:19:26 -0500

    fix bug

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -5,7 +5,26 @@ import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-post',
-  templateUrl: './post.component.html',
+  template: 
+  `<ng-container>
+    <div class="onepost" (click)="click($event)">
+        <div>Title: {{post.title}}</div>
+        <div>Content: {{post.content}}</div>
+        <div>Created: {{post.creationDate}}</div>
+        <form>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1"></label>
+                <textarea ng-click="$event.stopPropagation" style="z-index: 1000;" class="form-control" type="text" id="exampleFormControlTextarea1" rows="3" [(ngModel)]="content" name="content">Add a comment...</textarea>
+            </div>
+            <button class="btn btn-primary" (click)="createComment(post.id, content)">Post</button>
+        </form>
+        <div *ngIf="showComments && post.comments.length > 0">
+            <div style="border: 10px solid blue;" *ngFor="let comment of post.comments">
+                <app-post [showComments]="true" [post]="comment"></app-post>
+            </div>
+        </div>
+    </div>
+  </ng-container>`,
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
@@ -22,9 +41,10 @@ export class PostComponent implements OnInit {
     title: ''
   };
 
-  newContent: String = "";
+  @Input() showComments = false;
 
-  showComments = false;
+  content: String = "";
+
   constructor(private es: EventService, private ps: PostService) { }
 
   ngOnInit(): void {
@@ -33,7 +53,16 @@ export class PostComponent implements OnInit {
     })
   }
 
-  click() {
+  click(e: Event) {
+    e.stopPropagation();
+    e.preventDefault();
+    console.log('click got triggered!!');
+
+    console.log(e);
+    console.log(e.target);
+    console.log(this.post.comments);
+    console.log(this.post.comments.length);
+
     this.showComments = !this.showComments;
   }
 
