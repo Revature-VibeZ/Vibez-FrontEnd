8e229e68f3fadb885022eb788e8590e123902976
unknown <contact@kcsam.net>
2022-01-03 20:51:50 -0500

    Debugging post-feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,7 +38,6 @@ import { PostComponent } from './components/post/post.component';
     NavComponent,
     ProfileComponent,
     PostFeedComponent,
-    CommentComponent,
     PostComponent
   ],
   imports: [
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -9,11 +9,33 @@ export class PostService {
   posts : any = [];
   constructor(private http: HttpClient) { }
 
+  // login(data: any){
+  //   return this.http.post(`http://localhost:8080/api/auth`, data, {
+  //     headers: {'Content-type': 'application/json'},
+  //     observe: 'response'
+  //   }).pipe(
+  //     map(response => {
+  //       this.currentUser = response.body;
+  //       console.log(this.currentUser);
+  //       this.token = response.headers.get('Authorization') || '';
+  //       localStorage.setItem("token", this.token);
+  //       localStorage.setItem("currentUser", JSON.stringify(this.currentUser));
+  //     })
+  //   );
+  // }
+
+
   getAll() {
     console.log('inside post service ts get all');
 
     let url = 'http://localhost:8080/posts';
-    return this.http.get(url).pipe(map((response: any) => {
+    let headers : any = {
+      'Content-Type' : 'application/json',
+      'Access-Control-Allow-Origin' : '*',
+      'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+    }
+    return this.http.get(url, headers   
+      ).pipe(map((response: any) => {
       console.log("getAll function!")
       console.log(response)
       this.posts = response;
