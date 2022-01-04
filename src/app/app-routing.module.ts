bb455369f5583d17e296df1e59df6e875e2372b0
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2022-01-04 16:42:12 -0500

    clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './helpers/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { PostFeedComponent } from './components/post-feed/post-feed.component';
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: 'postfeed', component: PostFeedComponent}
   // { path: '', component: HomeComponent, canActivate: [AuthGuard] } Example of how to add authguard
 
   // { path: '**', redirectTo: '' }
