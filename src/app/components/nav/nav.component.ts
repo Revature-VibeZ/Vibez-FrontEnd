fbcc53dab807e771fb2be3d812d5df46391cccb3
brayden10 <56268765+brayden10@users.noreply.github.com>
2022-01-06 08:22:54 -0700

    a new nav bar is born

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,25 +6,27 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  constructor(private profileService: ProfileService, private router: Router, private as: AuthService) { }
+  constructor(
+    private profileService: ProfileService,
+    private router: Router,
+    private as: AuthService
+  ) {}
   sessionStorage = sessionStorage;
-  ngOnInit(): void {
-  }
-  search: String = "";
-
+  ngOnInit(): void {}
+  search: String = '';
 
   /*
   Saves username typed into search bar into the sessionStorage and then redirects to user profile.
   Will need some tweaking eventually.
   */
   searchByUsername(username: string) {
-    sessionStorage.setItem("username", username);
+    sessionStorage.setItem('username', username);
     this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
       this.router.navigate(['profile']);
-    })
+    });
   }
 
   /*
@@ -33,18 +35,35 @@ export class NavComponent implements OnInit {
   loadProfile() {
     this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
       this.router.navigate(['profile']);
-    })
+    });
   }
 
   loadPostFeed() {
     this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
       this.router.navigate(['postfeed']);
-    })
+    });
   }
 
   logout() {
     this.as.logout();
     this.router.navigate(['login']);
   }
+  
+  nav = document.querySelector('nav') as HTMLElement;
+  toggledText = "Menu";
 
+  toggleNav() {
+    if(document.querySelector('nav') as HTMLElement){
+      this.nav = document.querySelector('nav') as HTMLElement;
+    }
+    if(this.nav.classList.contains('close')){
+      this.nav.classList.toggle('open');
+      this.nav.classList.toggle('close');
+      this.toggledText = "Close";
+    }else{
+      this.nav.classList.toggle('open');
+      this.nav.classList.toggle('close');
+      this.toggledText = "Menu";
+    }
+  }
 }
