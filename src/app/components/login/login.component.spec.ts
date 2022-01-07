e20667742d443af8a8158537455199a166957736
tricialynn <plg1003@wildcats.unh.edu>
2022-01-07 16:06:34 -0500

    I broke something so the login and register component tests no longer work, but heres the rest of testing

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,25 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from 'src/app/services/auth.service';
 
 import { LoginComponent } from './login.component';
+
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ ReactiveFormsModule, FormsModule, HttpClientModule, RouterTestingModule],
+      declarations: [ LoginComponent ],
+      providers: [AuthService]
+    });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.ngOnInit();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
+
+  it('should call login method', async(() => {
+    let loginElement: DebugElement;
+    const DebugElement = fixture.debugElement;
+    let authService = DebugElement.injector.get(AuthService);
+    let loginSpy = spyOn(authService, 'login').and.callThrough();
+    loginElement = fixture.debugElement.query(By.css('form'));
+    component.form.controls['username'].setValue('username1');
+    component.form.controls['password'].setValue('password');
+    loginElement.triggerEventHandler('ngSubmit', null);
+    expect(loginSpy).toHaveBeenCalledTimes(1);
+  }));
 });
--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,18 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
 
 import { AuthService } from './auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule]
+    });
     service = TestBed.inject(AuthService);
   });
 
