5ab0b48b7c862b0591a9ae892091c497439642a1
tricialynn <plg1003@wildcats.unh.edu>
2022-01-10 14:50:14 -0500

    fixed tests for some services

--- src/app/components/create-post/create-post.component.spec.ts
+++ src/app/components/create-post/create-post.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CreatePostComponent } from './create-post.component';
 
@@ -8,6 +10,7 @@ describe('CreatePostComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ CreatePostComponent ]
     })
     .compileComponents();
